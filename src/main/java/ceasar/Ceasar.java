f6b1c1c4db4ae8add02537a24c1245a6574a3b4c
Mash2 <mashjosephk01@gmail.com>
2023-05-02 16:13:02 +0300

    code formated

--- src/main/java/ceasar/Ceasar.java
+++ src/main/java/ceasar/Ceasar.java
@@ -3,66 +3,30 @@ package ceasar;
 
 public class Ceasar {
 
-    public static String encrypt(String plainText,int shift) {
+    public static String encrypt(String plainText, int shift) {
         if (shift > 26) {
             shift = shift % 26;
         } else if (shift < 0) {
             shift = (shift % 26) + 26;
         }
-        String cipherText="";
-        int length=plainText.length();
-        for (int i=0;i<length;i++){
-            char ch=plainText.charAt(i);
-            if (Character.isLetter(ch));
-            if(Character.isLowerCase(ch)) {
+        String cipherText = "";
+        int length = plainText.length();
+        for (int i = 0; i < length; i++) {
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)) ;
+            if (Character.isLowerCase(ch)) {
                 char c = (char) (ch + shift);
                 if (c > 'z') {
                     cipherText += (char) (ch - (26 - shift));
                 } else {
                     cipherText += c;
                 }
-            }
-            else if(Character.isUpperCase(ch))
-                {
-                    char c=(char)(ch+ shift);
-                    if (c>'Z'){
-                        cipherText+=(char)(ch-(26-shift));
-                    }else{
-                        cipherText+=c;
-                    }
-            }
-
-        }
-
-            return cipherText;
-
-    }
-    public static String decrypt(String plainText,int shift) {
-        if (shift > 26) {
-            shift = shift % 26;
-        } else if (shift < 0) {
-            shift = (shift % 26) + 26;
-        }
-        String cipherText="";
-        int length=plainText.length();
-        for (int i=0;i<length;i++){
-            char ch=plainText.charAt(i);
-            if (Character.isLetter(ch));
-            if(Character.isLowerCase(ch)) {
-                char c = (char) (ch - shift);
-                if (c < 'a') {
-                    cipherText += (char) (ch + (26 - shift));
+            } else if (Character.isUpperCase(ch)) {
+                char c = (char) (ch + shift);
+                if (c > 'Z') {
+                    cipherText += (char) (ch - (26 - shift));
                 } else {
                     cipherText += c;
-                }
-            }
-            else if(Character.isUpperCase(ch))
-            {
-                char c=(char)(ch- shift);
-                if (c<'A'){
-                    cipherText+=(char)(ch+(26-shift));
-                }else{
-                    cipherText+=c;
                 }
             }
 
@@ -71,11 +35,45 @@ public class Ceasar {
         return cipherText;
 
     }
+
+    public static String decrypt(String plainText, int shift) {
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        String cipherText = "";
+        int length = plainText.length();
+        for (int i = 0; i < length; i++) {
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)) ;
+            if (Character.isLowerCase(ch)) {
+                char c = (char) (ch - shift);
+                if (c < 'a') {
+                    cipherText += (char) (ch + (26 - shift));
+                } else {
+                    cipherText += c;
+                }
+            } else if (Character.isUpperCase(ch)) {
+                char c = (char) (ch - shift);
+                if (c < 'A') {
+                    cipherText += (char) (ch + (26 - shift));
+                } else {
+                    cipherText += c;
+                }
+            }
+
+        }
+
+        return cipherText;
+
+    }
+
     public static void main(String[] args) {
-String text="THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-String cipher=encrypt(text,5);
+        String text = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        String cipher = encrypt(text, 5);
         System.out.println(cipher);
-String decrypted=decrypt(cipher,5);
+        String decrypted = decrypt(cipher, 5);
         System.out.println(decrypted);
     }
 }
